a83140b03ce7940534722f399b5b15551b4462db
zhiyu <519757634@qq.com>
2022-11-01 22:25:42 +0100

    update for models, data loader, cannot match the requirement

--- .ipynb_checkpoints/plot_utils-checkpoint.py
+++ .ipynb_checkpoints/plot_utils-checkpoint.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def plot_segmentations(im: np.ndarray, seg: np.ndarray, i: int = 65):
     _, ax = plt.subplots(1, 3, figsize=(15, 5))
 
-    ax[0].imshow(np.rot90(im[..., i], k=3), cmap='gray') #
+    ax[0].imshow(np.rot90(im[..., i], k=3), cmap='gray')
     ax[0].imshow(np.rot90(seg[..., i], k=3),
                  alpha=0.5 * (np.rot90(seg[..., i] > 0, k=3)),
                  interpolation=None, cmap='jet')
